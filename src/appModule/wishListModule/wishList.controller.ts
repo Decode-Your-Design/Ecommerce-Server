b3be4d29466c0204b890581da0fe621e29f18b92
MaxSingh01 <ranjitsingh131101@gmail.com>
2022-11-04 20:14:34 +0530

    change

--- src/appModule/wishListModule/wishList.controller.ts
+++ src/appModule/wishListModule/wishList.controller.ts
@@ -33,3 +33,29 @@ export const addProduct = async (req: any, res: Response) => {
     });
   }
 };
+
+export const getProduct = async (req: any, res: Response) => {
+  try {
+    const addedProduct = await await WishListModel.find({
+      user: req.body.user,
+    }).populate("product");
+    if (addedProduct) {
+      return res.status(200).json({
+        message: "Product fetched successfully",
+        result: addedProduct,
+        success: true,
+      });
+    } else {
+      return res.status(400).json({
+        message: "Failed to fetch product ",
+        success: false,
+      });
+    }
+  } catch (e) {
+    return res.status(200).json({
+      message: "Failed to add the product",
+      success: false,
+      error: e,
+    });
+  }
+};
--- src/appModule/wishListModule/wishList.routes.ts
+++ src/appModule/wishListModule/wishList.routes.ts
@@ -1,9 +1,12 @@
 import express, { Router } from "express";
 import { verifyJwtToken } from "../../utils/middleware/verify-jwt-token";
 
-import {addProduct} from "./wishList.controller";
+import {addProduct, getProduct} from "./wishList.controller";
 
 export const WishListRoutes: Router = express.Router();
 
 // /api/wishList/addProduct
 WishListRoutes.post("/addProduct", verifyJwtToken, addProduct);
+
+// /api/wishList/getProduct
+WishListRoutes.get("/getProduct", verifyJwtToken, getProduct);
