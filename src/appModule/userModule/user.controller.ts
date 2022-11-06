a60ac29c3df5d09f569559dd734ed5fb4ca541c5
MaxSingh01 <ranjitsingh131101@gmail.com>
2022-11-06 19:21:08 +0530

    changes

--- src/appModule/userModule/user.controller.ts
+++ src/appModule/userModule/user.controller.ts
@@ -39,141 +39,53 @@ export const addUser = async (req: any, res: Response) => {
   }
 };
 
-// export const signup = async (req: any, res: Response) => {
-//   try {
-//     const employee = await UserModel.findOne({
-//       phone: req.body.phone,
-//     });
-//     console.log(employee);
-//     if (employee) {
-//       return res.status(200).send({
-//         message:
-//           employee.email == req.body.email
-//             ? "Email already exist "
-//             : employee.phone == req.body.phone
-//             ? "phone number already exist"
-//             : "",
-//         success: false,
-//         result: employee,
-//       });
-//     } else {
-//       // req.body.accessControlList = JSON.parse(req.body.accessControlList);
-//       await UserModel.create({ ...req.body });
-//       return res.status(200).send({
-//         message: "Employee added successfully",
-//         success: true,
-//         result: employee,
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(200).send({
-//       success: false,
-//       message: "Failed to create the service",
-//       error: error,
-//     });
-//   }
-// };
-
-export const addToWishlist = async (req: any, res: Response) => {
-  const { productId } = req.params;
+export const fetchUserInfo = async (req: Request, res: Response) => {
   try {
-    // let r;
-    const productData = await ProductModel.findOne({
-      _id: "636400830fc1b9d1cd7e1f98",
+    const userInfo = await UserModel.find({
+      user: req.body.user,
     });
-    console.log("yhid",productData)
-    const userDetail = await UserModel.findByIdAndUpdate(req.body.user, {
-    $push:{ wishlist: productData},
-    });
-   
-    console.log(userDetail);
-  } catch (error) {
-    return res.status(500).json({
+    if (userInfo) {
+      return res.status(200).json({
+        message: "User information fetched successfully",
+        result: userInfo,
+        success: true,
+      });
+    } else {
+      return res.status(400).json({
+        message: "Failed to fetch user ",
+        success: false,
+      });
+    }
+  } catch (e) {
+    return res.status(200).json({
+      message: "Failed",
       success: false,
-      message: "Internal server error",
+      error: e,
     });
   }
 };
-// export const vendorSignup = async (req: Request, res: Response) => {
-//   try {
-//     const { email, phone } = req.body;
-//     const user = await UserModel.findOne({
-//       $or: [{ email: email }, { phone: phone }],
-//     });
-//     if (user) {
-//       return res.status(200).json({
-//         result: user,
-//         message:
-//           user.email == email
-//             ? "Email already exist"
-//             : user.phone == req.body.phone
-//             ? "phone number already exist"
-//             : "",
-//         success: false,
-//       });
-//     } else {
-//       await UserModel.create({ ...req.body });
-//       return res.status(200).send({
-//         message: "Vendor added successfully",
-//         success: true,
-//         result: user,
-//       });
-//     }
-//   } catch (e) {
-//     return res.status(200).json({
-//       success: false,
-//       error: e,
-//     });
-//   }
-// };
 
-// export const verifyOtp = async (req: Request, response: Response) => {
-//   try {
-//     const data = new UserModel(req.body);
-//     await data.save();
-//   } catch (error) {
-//     console.log("error calling vendor sign  up api", error.message);
-//   }
-// };
-
-// export const vendorLogin = async (req: Request, res: Response) => {
-//   const { email, password } = req.body;
-//   UserModel.find({ email: email, password: password }, function (err, res) {
-//     if (res.length > 0) {
-//       console.log("User logged in successfully");
-//     } else {
-//       console.log("User is not authenticated");
-//     }
-//   });
-// };
-
-// export const vendorAddProduct = async (req: Request, res: Response) => {
-//   const { vendorId } = req.params;
-//   const query = { _id: vendorId };
-//   try {
-//     const product = {
-//       name: "test product ",
-//     };
-//     await UserModel.updateOne(query, { $push: { products: product } });
-//   } catch (error) {
-//     console.log("error calling vendor sign  up api", error.message);
-//   }
-// };
-
-// export const getVendors = async (req: Request, res: Response) => {
-//   try {
-//     const data = await UserModel.find({});
-//   } catch (error) {
-//     console.log("error calling vendor sign  up api", error.message);
-//   }
-// };
-
-// export const vendorGetProducts = async (req: Request, res: Response) => {
-//   const { vendorId } = req.params;
-//   try {
-//     const data = await UserModel.find({ _id: vendorId });
-//   } catch (error) {
-//     console.log("error calling vendor sign  up api", error.message);
-//   }
-// };
+export const updateUserInfo = async (req: Request, res: Response) => {
+  try {
+    const { _id } = req.body;
+    const updatedUser = await UserModel.findByIdAndUpdate(_id, { ...req.body });
+    if (updatedUser) {
+      return res.status(200).json({
+        message: "User information updated successfully",
+        result: updatedUser,
+        success: true,
+      });
+    } else {
+      return res.status(400).json({
+        message: "Failed to updated user ",
+        success: false,
+      });
+    }
+  } catch (e) {
+    return res.status(200).json({
+      message: "Failed",
+      success: false,
+      error: e,
+    });
+  }
+};
--- src/appModule/userModule/user.routes.ts
+++ src/appModule/userModule/user.routes.ts
@@ -1,17 +1,13 @@
 import express, { Router } from "express";
 import { verifyJwtToken } from "../../utils/middleware/verify-jwt-token";
-import { addUser,addToWishlist } from "./user.controller";
+import { addUser, fetchUserInfo, updateUserInfo } from "./user.controller";
 export const userRoutes: Router = express.Router();
 
-// /api/product/getAllProducts
-
 // /api/users/addUser
-userRoutes.post("/addUser",addUser)
-userRoutes.post("/addToWishList/:productId",verifyJwtToken,addToWishlist)
+userRoutes.post("/addUser", addUser);
 
+// /api/users/fetchUserInfo
+userRoutes.post("/fetchUserInfo", verifyJwtToken, fetchUserInfo);
 
-// userRoutes.post("/vendor/signup", vendorSignup);
-// userRoutes.post("/vendor/login", vendorLogin);
-// userRoutes.post("/vendor/addProduct/:vendorId", vendorAddProduct);
-// userRoutes.post("/vendor/getAllVendors", getVendors);
-// userRoutes.post("/vendor/products/:vendorId", vendorGetProducts);
+// /api/users/updateUserInfo
+userRoutes.post("/updateUserInfo", verifyJwtToken, updateUserInfo);
